0847599d0644b4f8bbfabd2bbc8b8463789d8dc6
Semyon Sporysh <beee.semok@gmail.com>
2020-09-21 10:58:11 +0300

    Added Reset controller with needed forms, twig and tokenizer and services settings for autowiring

---
+++ manager/src/Controller/Auth/ResetController.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Auth;
+
+use App\Model\User\UseCase\Reset;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResetController extends AbstractController
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @Route("/reset", name="auth.reset")
+     */
+    public function request(Request $request, Reset\Request\Handler $handler): Response
+    {
+        $command = new REset\Request\Command();
+
+        $form = $this->createForm(Reset\Request\Form::class, $command);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                $this->addFlash('success', 'Check your email please.');
+                return $this->redirectToRoute('/');
+            } catch (\DomainException $e) {
+                $this->logger->error($e->getMessage(), ['exception' => $e]);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('app/auth/reset/request', [
+           'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/reset/{token}, name="auth.reset.reset")
+     */
+    public function reset(string $token, Request $request, Reset\Reset\Handler $handler): Response
+    {
+        $command = new Reset\Reset\Command();
+
+        $form = $this->createForm(Reset\Reset\Form::class, $command);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                $this->addFlash('success', 'Password is successfully changed.');
+                return $this->redirectToRoute('/');
+            } catch (\DomainException $e) {
+                $this->logger->error($e->getMessage(), ['exception' => $e]);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('/app/auth/reset/reset', [
+            'form' => $form->createView()
+        ]);
+    }
+}
